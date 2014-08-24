ce117b84696a2282c0313d8bbfa07ee30cf3cd85
j3ck <fox_raiodin@mail.ru>
2014-08-24 18:39:29 +0300

    added sign_in through facebook

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -233,6 +233,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
+  config.omniauth :facebook, "504207406348774", "c12d94f4ff2cbdc73a364f4a646cb683"
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   resources :types
   resources :categories
 
-  devise_for :users, path: ""
+  devise_for :users, path: "", :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   resources :adverts do
     member do
       get 'in_moderate'
@@ -16,7 +16,7 @@ Rails.application.routes.draw do
       get 'in_publish'
     end
   end
-
+  
   root 'adverts#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".

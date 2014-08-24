42b80c3ff20cdcb992a73639be6440b8ef49a390
j3ck <fox_raiodin@mail.ru>
2014-08-24 21:58:53 +0300

    added whenever tasks

--- app/models/image.rb
+++ app/models/image.rb
@@ -2,4 +2,9 @@ class Image < ActiveRecord::Base
   has_attached_file :data, :styles => { :small => "100x100#", :medium => "300x300#" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :data, :content_type => /\Aimage\/.*\Z/
   belongs_to :advert
+
+
+  def self.destroy_images_a_without_advert_id
+    Image.where(advert_id: nil).delete_all
+  end
 end
--- config/schedule.rb
+++ config/schedule.rb
@@ -1,20 +1,10 @@
-# Use this file to easily define all of your cron jobs.
-#
-# It's helpful, but not entirely necessary to understand cron before proceeding.
-# http://en.wikipedia.org/wiki/Cron
+set :output, "#{path}/log/cron.log"
+set :environment, "development"
 
-# Example:
-#
-# set :output, "/path/to/my/cron_log.log"
-#
-# every 2.hours do
-#   command "/usr/bin/some_great_command"
-#   runner "MyModel.some_method"
-#   rake "some:great:rake:task"
-# end
-#
-# every 4.days do
-#   runner "AnotherModel.prune_old_records"
-# end
+every 10.minutes do
+  runner "Advert.to_archive_old_adverts"
+end
 
-# Learn more: http://github.com/javan/whenever
+every 1.day, at: '00:00 am' do
+  runner "Image.destroy_images_a_without_advert_id"
+end

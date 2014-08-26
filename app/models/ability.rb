b3c2540d52dcdb293d9dec74926fd02412df105d
j3ck <fox_raiodin@mail.ru>
2014-08-26 15:26:46 +0300

    added Cancancan abilities logic

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,32 +1,24 @@
 class Ability
   include CanCan::Ability
 
-  def initialize(user)
-    # Define abilities for the passed in user here. For example:
-    #
-    #   user ||= User.new # guest user (not logged in)
-    #   if user.admin?
-    #     can :manage, :all
-    #   else
-    #     can :read, :all
-    #   end
-    #
-    # The first argument to `can` is the action you are giving the user
-    # permission to do.
-    # If you pass :manage it will apply to every action. Other common actions
-    # here are :read, :create, :update and :destroy.
-    #
-    # The second argument is the resource the user can perform the action on.
-    # If you pass :all it will apply to every resource. Otherwise pass a Ruby
-    # class of the resource.
-    #
-    # The third argument is an optional hash of conditions to further filter the
-    # objects.
-    # For example, here the user can only update published articles.
-    #
-    #   can :update, Article, :published => true
-    #
-    # See the wiki for details:
-    # https://github.com/CanCanCommunity/cancancan/wiki/Defining-Abilities
+  def initialize(user, admin, namespace)
+    if namespace == "Admin"
+      if admin.kind_of? Admin
+        can [:index, :in_publish, :in_reject], Advert
+        can [:index, :new, :edit, :create, :update], Category
+        can [:index, :user_adverts_with_state ], User
+      end
+    else
+      if user.kind_of? User
+        can [:index, :new, :create, :show], Advert
+        can [:edit, :update], Advert, user_id:  user.id
+        can :in_moderate, Advert, user_id:  user.id, state: "template"
+        can :in_archive, Advert, user_id:  user.id, state: "published"
+        can :in_newest, Advert, user_id:  user.id, state: ["published", "rejected", "moderated", "archived"]
+
+        can :create, Image
+        can :destroy, Image, advert_id: user.advert_ids
+      end
+    end
   end
 end

d0d10b7f8fa11266860ae3ece304408784ecee7d
j3ck <fox_raiodin@mail.ru>
2014-08-26 21:54:31 +0300

    processing 'cancancan' exception

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,11 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  rescue_from CanCan::AccessDenied do |exception|
+    flash[:error] = "Access denied"
+    redirect_to root_url, :alert => exception.message
+  end
+
   private
     def current_ability
       controller_name_segments = params[:controller].split('/')

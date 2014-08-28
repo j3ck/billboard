0cc303c26ef6f6a0c98aa61211210246ad345b5c
j3ck <fox_raiodin@mail.ru>
2014-08-28 23:08:10 +0300

    resize medium style for image

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,5 +1,5 @@
 class Image < ActiveRecord::Base
-  has_attached_file :data, :styles => { :small => "100x100#", :medium => "300x300#" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :data, :styles => { :small => "100x100#", :medium => "200x200#" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :data, :content_type => /\Aimage\/.*\Z/
   belongs_to :advert
 

25e44bbc6eaaacea649aee88c054228257848f9b
j3ck <fox_raiodin@mail.ru>
2014-09-04 15:00:20 +0300

    remove query from home views

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,7 @@ class HomeController < ApplicationController
     @adverts = Advert.includes(:category, :type).where(state: :published)
     @search = @adverts.search(params[:q])
     @adverts = @search.result(distinct: true).paginate(page: params[:page], per_page: 5)
+    @categories = Category.order(created_at: :asc)
+    @types = Type.all
   end
 end

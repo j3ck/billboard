947004e537de83215ef294397b95f5871c5c6ba7
j3ck <fox_raiodin@mail.ru>
2014-09-07 21:14:07 +0300

    includes for remove n+1 query

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -7,7 +7,7 @@ class Admin::UsersController < Admin::AdminController
   end
 
   def user_adverts_with_state
-    @adverts = Advert.where(user_id: params[:id], state: params[:state])
+    @adverts = Advert.includes(:category, :type).where(user_id: params[:id], state: params[:state])
     adverts = render_to_string( partial: 'admin/users/user_adverts', :formats => [:html], locals: { adverts: @adverts } )
     respond_with :html => adverts
   end
